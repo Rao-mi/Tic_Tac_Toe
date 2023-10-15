aea47ae88234c75f2bb9bef53d1692bcc4ec9e25
Rao-mi <mariospaziante99@gmail.com>
2023-10-15 22:37:22 +0200

    Aggiustata funzione line_check e aggiunta all_plays()

--- Tic_Tac_Toe.cpp
+++ Tic_Tac_Toe.cpp
@@ -8,6 +8,9 @@ using std::cin;
 using std::endl;
 
 
+//--------------------------------------FUNZIONI NON PUBBLICHE------------------------------------------
+
+
 
 // Funzione che mi conta i tipi
 int type_count (state type, std::vector<state> grid) {
@@ -19,7 +22,7 @@ int type_count (state type, std::vector<state> grid) {
 }
 
 // Funzione che verifica sulla direzione data tutte le celle
-bool line_check (std::vector<state>& grid, int cell_number, int dimension, int line, int start, std::vector<std::vector<int>> constrain) {
+bool line_check (std::vector<state>& grid, int cell_number, int dimension, int line, int start,std::vector<int> v_dir, std::vector<std::vector<int>> constrain) {
     int u = line;
     int flag=1;
     state player =grid[start];
@@ -29,11 +32,16 @@ bool line_check (std::vector<state>& grid, int cell_number, int dimension, int l
          }
         else if (grid[start+u]==player) { 
             for (int i=0; i<static_cast<int>(constrain.size()); i++) {
-            for (int j=0; j<static_cast<int>(constrain[i].size()); j++) {
-                if ((k_module(int_pow(cell_number,i+1-dimension),start+u)==constrain[i][j] && i>=dimension)||(k_module(int_pow(cell_number,i+1),start+u)==constrain[i][j] && i<dimension)) {
+            for (int j=0; j<static_cast<int>(constrain[i].size()); j++) {      
+            if ((k_module(int_pow(cell_number,i+1-dimension),start+u)==constrain[i][j] && i>=dimension)||(k_module(int_pow(cell_number,i+1),start+u)==constrain[i][j] && i<dimension)) {
+                    std::vector<int> check;
+                    if (i>=dimension){check = canonic(cell_number-1,i);}
+                    else {check = inv_canonic(cell_number-1,i);}
+                    if (scalar_p(check,v_dir)>0) {
                     player=state::N;
                     j=constrain[i].size()-1;
                     i=constrain.size()-1;
+                    }                
                 }   
             }}        
             u+=u;
@@ -63,7 +71,7 @@ bool check_win(std::vector<state> grid, int cell_number, int dimension) {
                     }}
             n_directions = numeric_directions(cell_number,directions); // Trasformo i vettori in numeri
                        for (size_t k=0; k<n_directions.size(); k++){
-                            if (grid[i+n_directions[k]]==player && line_check(grid,cell_number,dimension,n_directions[k],i, limit)) //qui faccio check della direzione partendo dalla cella grid[i+n_directions[k]], solo se player!=N
+                            if (grid[i+n_directions[k]]==player && line_check(grid,cell_number,dimension,n_directions[k],i,directions[k],limit)) //qui faccio check della direzione partendo dalla cella grid[i+n_directions[k]], solo se player!=N
                             {
                                 return true;
                             }}
@@ -75,13 +83,11 @@ bool check_win(std::vector<state> grid, int cell_number, int dimension) {
     }
      return false;
 }
+//--------------------------------------CLASSE BOARD------------------------------------------
 
 Board::Board (int set_cell_n,int set_dim)
     : cell_number(set_cell_n), dimension(set_dim), 
     grid(int_pow(set_cell_n,set_dim),state::N) {}
-
-
-
 
 bool Board::Move(state player) {
     bool win;
@@ -145,8 +151,42 @@ void Board::print2D() {
         }
     }    
 }
+// Funzione ricorsiva per generare le mosse
+void generate_moves(std::vector<state> grid, int cell_number, int dimension, std::vector<std::vector<std::vector<state>>>& result, int move) {
+    if (move >= int_pow(cell_number, dimension)) {
+        return; // Se tutte le mosse sono state fatte, esci dalla ricorsione
+    }
+        
+    if (move==0) {
+        std::vector<std::vector<state>> start;
+        start.push_back(grid);
+        result.push_back(start);
+    }
+    else{
+        std::vector<std::vector<state>> collector;
+    for (size_t i=0; i<result[move-1].size(); i++) {
+    for (int j = 0; j < int_pow(cell_number, dimension); j++) {
+        if (result[move-1][i][j] == state::N && !check_win(result[move-1][i],cell_number,dimension)) {
+           std::vector<state> grid=result[move-1][i];
+            // Effettua la mossa
+            grid[j] = (move % 2 == 0) ? state::X : state::O;
+            // Copia la griglia attuale nel vettore dei risultati
+            collector.push_back(grid);
+            // Annulla la mossa
+            grid[j] = state::N; 
+        }
+    }}
+    result.push_back(collector);
+    }
+    generate_moves(grid, cell_number, dimension, result, move + 1);
+}
+    std::vector<std::vector<std::vector<state>>> Board::all_plays() {
+       std::vector<std::vector<std::vector<state>>> result; 
+        // Richiama la funzione ricorsiva per generare le mosse
+        generate_moves(grid, cell_number, dimension, result, 0);
 
-
+        return result;
+    }
 //---------------------------------NOTES---------------------------
 /* OLD LINE CHECK 
     int stop=0;
