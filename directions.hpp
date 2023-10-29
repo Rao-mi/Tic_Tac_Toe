d0a8f707f9c562bc842ab8297550d6af57f55484
Rao-mi <mariospaziante99@gmail.com>
2023-10-29 19:22:18 +0100

    Introduzione di alcune funzioni di supporto aggiuntive

--- directions.hpp
+++ directions.hpp
@@ -17,9 +17,14 @@ int k_module(int k, int evaluate);
 // Funzione per stampare le direzioni
 void print( std::vector<std::vector<int>>& combinazioni);
 void print( std::vector<int>& combinazioni);
+
 // Funzione prodotto scalare vettoriale
 int scalar_p(const std::vector<int>& vector1, const std::vector<int>& vector2);
 
+// Booleano che verifica se un dato int appartiene a un dato vector<int> o vector<vector<int>>
+bool element_verify (int element, const std::vector<int>& row);
+//generalizzazione per vector<vector<int>>
+bool element_verify (int i, const std::vector<std::vector<int>>& limit,int cell_number, int dimension);
 //---------------------------------------------------------------------------------
 // Parte di selezione
 
@@ -32,6 +37,8 @@ bool zero_check (const std::vector<int>& check_d);
 // Funzione per togliere lo zero
 std::vector<std::vector<int>> zero_cut(std::vector<std::vector<int>> directions);
 
+// Funzione di verifica angoli
+bool is_corner(int cell_number, int dimension, int i);
 //----------------------------------------------------------------------------------------
 //Parte di generazione
 
@@ -39,8 +46,11 @@ std::vector<std::vector<int>> zero_cut(std::vector<std::vector<int>> directions)
 void comb_gen(std::vector<int>& combination, int k, std::vector<std::vector<int>>& risultato, int posizione); 
 
 // Funzione che genera il vettore i-esimo canonico di dim cell_number 
-std::vector<int> canonic(int cell_number, int i);
-std::vector<int> inv_canonic(int cell_number, int i);
+std::vector<int> canonic(int dimension, int i);
+std::vector<int> inv_canonic(int dimension, int i);
+
+//Funzione per generare gli angoli
+std::vector<int> corner_generator (int cell_number, int dimension, int first_corner,std::vector<int>& collector);
 //-----------USO ESTERNO------------------------------------------------------------------
 
 // Funzione per trasformare le direzioni nei numeri equivalenti
@@ -61,5 +71,7 @@ if (k_module(int_pow(cell_number,k+1),j+int_pow(cell_number,i))==costrains[k] &&
 else if (k_module(int_pow(cell_number,k+1),j+int_pow(cell_number,i))==costrains[k] && k<i)){directions = j_a_remover(directions,k,-1);}*/
 std::vector<std::vector<int>> constrains (int cell_number, int dimension);
 
+// Funzione restituisce i numeri delle celle non angolari e non interne
+std::vector<std::vector<std::vector<int>>> hyper_constrains (std::vector<std::vector<int>> constrains,int cell_number, int dimension);
 
 #endif
