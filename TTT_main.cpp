cc54b80997ca531af0a3e39c3e10cd517aacfa04
Rao-mi <mariospaziante99@gmail.com>
2024-01-04 18:31:08 +0100

    Migliorie estetiche

--- TTT_main.cpp
+++ TTT_main.cpp
@@ -8,10 +8,9 @@ using std::endl;
 
 
 int main() {
-    Board tavola(3,2);
-    std::vector<std::vector<std::vector<state>>> history;
-    history=tavola.all_plays();
     Board tavolozza(3,2);
+    std::vector<std::vector<std::vector<state>>> history;
+    history=tavolozza.all_plays();
     std::vector<int> wins;
     std::vector<std::vector<int>> win_traker;
 
@@ -29,26 +28,6 @@ int main() {
         }
         win_traker.push_back(wins);
         }
-    /*for (size_t j=0; j<history.size(); j++) {
-        std::cout<<"Alla mossa "<<j<<" abbiamo le seguenti possibilità"<<std::endl;
-        for (size_t k=0; k<history[j].size(); k++) {
-            std::cout<<"Possibilità n "<<k<<std::endl;
-        for (size_t i=0; i<history[j][k].size(); i++) {
-           // for (size_t l=0; l<history[j][k][i].size(); l++) {
-        if (i==2 || i==5 || i==8) {
-            if (history[j][k][i]==state::X) {cout<<" X "<<endl;}
-            else if (history[j][k][i]==state::O) {cout<<" O "<<endl;}
-            else if (history[j][k][i]==state::N) {cout<<"[ ]"<<endl;}
-        }
-        else {
-            if (history[j][k][i]==state::X) {cout<<" X ";}
-            else if (history[j][k][i]==state::O) {cout<<" O ";}
-            else if (history[j][k][i]==state::N) {cout<<"[ ]";}
-        }
-    }    
-        }
-        std::cout<<"--------------------------------------------------------------------"<<std::endl;
-        }*/
      std::ofstream file1("TTT_ENDS.csv");
         if (file1.is_open()) {
         for (size_t j=0; j<history.size(); j++) {
